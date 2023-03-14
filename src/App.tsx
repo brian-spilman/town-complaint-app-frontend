a99e43d127bb611ce742f7b6fef7ac9dfc7af67d
brian.spilman <brian.spilman@wsu.edu>
2023-03-14 12:01:15 -0700

    start of file setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/home-page';
 
 function App() {
 
   return <>
 
-    <h1>Initial Site</h1>
-  
+    <BrowserRouter>
+      <Routes>
+
+        <Route path='/' element={<HomePage/>} />
+
+      </Routes>
+    </BrowserRouter>
   </>
 
 }
